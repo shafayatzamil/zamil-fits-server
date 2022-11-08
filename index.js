89d71cdaa73d166c948b47993ac6ac0083513817
shafayat zamil <zamilkhan111.za@gmail.com>
2022-11-08 20:33:30 +0600

    server is connected on database

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
 require("colors")
 const app= express();
 
 
+const uri = `mongodb://localhost:27017`;
+const client= new MongoClient(uri);
+
+async function dbConnect() {
+    try {
+      await client.connect();
+      console.log("Database connected".yellow.italic);
+    } catch (error) {
+      console.log(error.name.bgRed, error.message.bold);
+    }
+  }
+  dbConnect();
 
 
 app.listen(5000,()=>{
